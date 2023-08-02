ef26041778e275781edf4960c2782d2968081a02
Valkiria Faria <val_nonato@hotmail.com>
2023-08-02 17:15:32 -0300

    properties, Game, ORM

--- src/test/java/com/devsuperior/dslist/DslistApplicationTests.java
+++ src/test/java/com/devsuperior/dslist/DslistApplicationTests.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dslist;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class DslistApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
 	}
 
 }
